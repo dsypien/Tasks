cf441659bb2e2e86c53c4260fb3058ef3e643383
David Sypien <sypiend@mskcc.org>
2015-03-03 19:17:48 -0500

    Get rid of dom manipulation from controller, create directive for it instead

--- app/scripts/controllers/tasks.js
+++ app/scripts/controllers/tasks.js
@@ -5,12 +5,6 @@ angular.module('tasksApp')
     $scope.project = projectStoreService.getCurrent();
     console.log('current proj: ' + $scope.project);
 
-    $scope.$on('$viewContentLoaded', function(){
-        jQuery('textarea').each(function(){
-            updateTextAreaHeight();
-        });
-    });
-
     $scope.addTask = function(){
     	$scope.project.tasks.push($scope.project.task);
     	$scope.project.task = '';
@@ -24,14 +18,4 @@ angular.module('tasksApp')
     $scope.$watch('project', function(){
     	projectStoreService.save($scope.project);
     }, true);
-
-    // var updateTextAreaHeight = function(){
-    // 	jQuery(this).height(0).height(this.scrollHeight);
-    // };
-
-    // function init(){
-    // 	jQuery('.tasklist').on('change keyup keydown paste cut', 'textarea', updateTextAreaHeight).find('textarea').change();	
-    // }
-    
-    init();
   });
---
+++ app/scripts/directives/textareaFitContent.js
@@ -0,0 +1,32 @@
+angular.module('tasksApp')
+	.directive('textareaFitContent', function($window){
+		function resizeTextArea(element){
+			$(element).height(0).height( $(element)[0].scrollHeight );
+		}
+
+		return{
+			// Restrict directive to attributes
+			restrict: 'A', 
+			link: function(scope, elem, attrs){
+				// Resize text area on key up
+				elem.bind('keyup', function(event){
+					var element = event.target;
+					resizeTextArea(element);
+				});
+
+				// Resize text area on window resize
+				angular.element($window).bind('resize', function() {
+					var element = elem;
+					scope.$apply(function(){
+						resizeTextArea(element);
+					});
+				});
+
+				// Resize text area as soon as element is added to DOM
+				setTimeout(function(){
+					var element = elem;
+					resizeTextArea(element);
+				});
+			}
+		};
+	});
