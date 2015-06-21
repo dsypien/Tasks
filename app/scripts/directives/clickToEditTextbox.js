adc9ae2060946e145aa2e07af8308397b63cc009
David Sypien <davidsypien@gmail.com>
2015-06-21 11:05:33 -0400

    update nav menu

--- app/scripts/directives/clickToEditTextbox.js
+++ app/scripts/directives/clickToEditTextbox.js
@@ -2,10 +2,11 @@
 
 angular.module('tasksApp')
 	.directive('clickEditTextbox', function(){
-		function controller($scope, $element){
+		function controller($scope){
 			$scope.isReadOnly = true;
 			$scope.enableEdit = function(){
 				$scope.isReadOnly = false;
+				angular.element('#editable_proj_name').trigger('focus');
 			};
 			$scope.disableEdit = function(){
 				$scope.isReadOnly = true;
@@ -14,11 +15,6 @@ angular.module('tasksApp')
 
 		return{
 			restrict: 'E',
-			link: function(scope, elem, attrs){
-				elem.bind('onblur', function(event){
-
-				});
-			},
 			controller: controller,
 			replace: true,
 			templateUrl: '/scripts/directives/clickToEditTextbox.html'
