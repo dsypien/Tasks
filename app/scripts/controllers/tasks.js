c76537dcd031721fc788b104edde66247d296475
David Sypien <davidsypien@gmail.com>
2015-08-20 21:09:13 -0400

    ability to move tasklists by drag and drop

--- app/scripts/controllers/tasks.js
+++ app/scripts/controllers/tasks.js
@@ -4,7 +4,12 @@ angular.module('tasksApp')
 
     $scope.project = projectStoreService.getCurrent();
     $scope.isOneColumn = window.innerWidth < 768;
-    $scope.isReadOnlyEditProjectName = true;
+    $scope.isReadOnlyEditProjectName = true;    
+    
+    var _dragThreshold = 0,
+        _isDraggedOverLeft = true,
+        _isDragging = false;
+
 
     $scope.$watch(function(){
         return window.innerWidth;
@@ -43,29 +48,39 @@ angular.module('tasksApp')
         };
     }
 
-    function drag(event){
-        event.dataTransfer.setData("text", event.target.id);
-    }
-
-    function drop(event){
-        event.preventDefault();
-        var draggedID = event.dataTransfer.getData("text");
-        var droppedID = closest(event.target, 'list_container').id;
-
-        var draggedIndex = getIndexOfListId(draggedID);
-        var droppedIndex = getIndexOfListId(droppedID);
-
-        console.log('moving ' + draggedIndex + ' to ' + droppedIndex);
-    }
-    
-
     function getIndexOfListId(id){
         return new Number( id.replace(/list_/, '') );
     }
 
-    function allowDrop(ev) {
-        console.log(ev);
-        ev.preventDefault();
+    // function swapLists(fromIndex, toIndex){
+    //     console.log("Before " + $scope.project.listAryNames[fromIndex] );
+    //     var fromList = $scope.project.listAry[fromIndex];
+    //     var fromListName = $scope.project.listAryNames[fromIndex];
+
+    //     $scope.project.listAry[fromIndex] = $scope.project.listAry[toIndex];
+    //     $scope.project.listAryNames[fromIndex] = $scope.project.listAryNames[toIndex];
+
+    //     $scope.project.listAry[toIndex] = fromList;
+    //     $scope.project.listAryNames[toIndex] = fromListName;
+    // }
+
+    function moveLists(fromIndex, toIndex){
+        var fromList = $scope.project.listAry[fromIndex],
+            fromListName = $scope.project.listAryNames[fromIndex],
+            listAry = $scope.project.listAry,
+            listAryNames = $scope.project.listAryNames;
+
+        if(toIndex > listAryNames.length){
+            listAry.push(undefined);
+            listAryNames.push(undefined);
+        }
+
+        listAry.splice(toIndex, 
+                       0,
+                       listAry.splice(fromIndex, 1)[0]);
+        listAryNames.splice(toIndex,
+                            0,
+                            listAryNames.splice(fromIndex, 1)[0]);
     }
 
     function closest(elem, className){
@@ -78,9 +93,56 @@ angular.module('tasksApp')
         return  null;
     }
 
-    $scope.drag = drag;
-    $scope.allowDrop = allowDrop;
-    $scope.drop = drop;
+    $scope.drag = function(event){
+        event.dataTransfer.setData("text", event.target.id);
+        _isDragging = true;
+    }
+
+    $scope.dragEnter = function(event){
+        var curElement = event.target;
+        do{
+            if(curElement.className.indexOf('list_container') > -1){
+                _dragThreshold = curElement.clientWidth / 2;
+
+                event.stopPropagation();
+                break;
+            }
+        }while(curElement = curElement.parentElement)
+    }
+
+    $scope.dragOver = function(event) {
+        if(event.offsetX > _dragThreshold){
+            _isDraggedOverLeft = false;
+        }
+        else{
+            _isDraggedOverLeft = true;
+        }
+
+        event.preventDefault();
+    }
+
+    $scope.drop = function (event){
+        var hoveringOverID = closest(event.target, 'list_container').id,
+            draggedID = event.dataTransfer.getData("text"),
+            overIndex = getIndexOfListId(hoveringOverID),
+            draggedIndex = getIndexOfListId(draggedID);
+
+        if(draggedIndex != overIndex){
+            $scope.$apply( function(){
+                console.log('moving index ' + draggedIndex + ' to index ' + overIndex);
+                moveLists(draggedIndex, overIndex);
+            });
+        }
+
+        if(_isDraggedOverLeft){
+            console.log("Dropping to the left");
+        }else{
+            console.log("Dropping to the right");
+        }
+
+        _isDragging = false;
+        event.preventDefault();
+    }
 
     $scope.sortableOptionsList = [createOptions('toDo'), createOptions('working'), createOptions('finished')];
 
